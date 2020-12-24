14d5bb9317a61335546b1edbf5b7350c7d282841
Vũ Trần Quốc Huy (Derek) <38975475+vtqhuy1998@users.noreply.github.com>
2020-12-24 22:28:49 +0700

    Cập nhật DTO

---
+++ STUDENT/STUDENT/DTO/THAMSODTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUDENT.DTO
+{
+    public class THAMSODTO
+    {
+        private int tuoiHocSinhToiThieu;
+        private int tuoiHocSinhToiDa;
+        private int siSoToiDa;
+        private int soLuongMonHoc;
+        private int diemDat;
+
+        public int TuoiHocSinhToiThieu { get => tuoiHocSinhToiThieu; set => tuoiHocSinhToiThieu = value; }
+        public int TuoiHocSinhToiDa { get => tuoiHocSinhToiDa; set => tuoiHocSinhToiDa = value; }
+        public int SiSoToiDa { get => siSoToiDa; set => siSoToiDa = value; }
+        public int SoLuongMonHoc { get => soLuongMonHoc; set => soLuongMonHoc = value; }
+        public int DiemDat { get => diemDat; set => diemDat = value; }
+    }
+}
